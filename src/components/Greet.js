7c9e450b0b6afc3c9d920260526ed6117d1a3508
AMANPATEL <amanpatel8844382@gmail.com>
2024-08-04 11:23:33 +0530

    Hello World Project In React

---
+++ src/components/Greet.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+// function Greet() {
+//   return <h1>Hello Aman</h1>;
+// }
+
+// export const Greet = () => {
+//   return (
+//     <div>Greet</div>
+//   )
+// }
+
+const Greet = () => <h1>Hello Aman</h1>;
+
+export default Greet;
