269de999f6bc0afdede3856a502403c35a315b6e
AMANPATEL <amanpatel8844382@gmail.com>
2024-08-05 14:02:04 +0530

    Props and State Basics Examples like increment ,Subscribe Button

--- src/App.js
+++ src/App.js
@@ -3,15 +3,33 @@ import "./App.css";
 import Greet from "./components/Greet";
 import Welcome from "./components/Welcome";
 import Hello from "./components/Hello";
+import React, { Component } from "react";
+import Message from "./components/Message";
+import Counter from "./components/Counter";
 
-function App() {
-  return (
-    <div className="App">
-      <Greet />
-      <Welcome />
-      <Hello />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Counter />
+        <Greet name="Jay" heroName="superman"></Greet>
+        <Welcome name="Jay" heroName="flash" />
+        {/* <Message /> */}
+        {
+          /* 
+          <p>this is Children Props</p>
+        </Greet>
+        <Greet name="RS" heroName="superman">
+          <button>Action</button>
+        </Greet>*/
+          // <Welcome name="GT" heroName="batman" />
+          // <Welcome name="RS" heroName="superman" />
+          /* 
+  <Welcome name="Jay" heroName="flash" /> */
+          // <Greet name="Jay" heroName="flash" />
+        }
+      </div>
+    );
+  }
 }
-
 export default App;
--- src/components/Greet.js
+++ src/components/Greet.js
@@ -10,10 +10,18 @@ import React from "react";
 //   )
 // }
 
-const Greet = () => (
-  <div>
-    <h1>Hello Aman</h1>
-  </div>
-);
+const Greet = (props) => {
+  const { name, heroName } = props;
+  // console.log(props);
+
+  return (
+    <div>
+      <h1>
+        Hello {name} a.k.a {heroName}
+      </h1>
+      {/* {props.children} */}
+    </div>
+  );
+};
 
 export default Greet;
