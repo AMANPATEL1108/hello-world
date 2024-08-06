49ebb956c07f2628d177c30c2683ff96924ee659
AMANPATEL <amanpatel8844382@gmail.com>
2024-08-06 10:57:17 +0530

    Basics Css impoet is Completed

--- src/App.js
+++ src/App.js
@@ -12,12 +12,19 @@ import EventBind from "./components/EventBind";
 import ParentComponent from "./components/ParentComponent";
 import UserGreeting from "./components/UserGreeting";
 import NameList from "./components/NameList";
-
+import Stylesheet from "./components/Stylesheet";
+import Inline from "./components/Inline";
+import "./appStyle.css";
+import styles from "./appStyle.module.css";
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <NameList />
+        <h1 className="error">Error </h1>
+        <h1 className={styles.success}>Sucess</h1>
+        {/* <Inline /> */}
+        {/* <Stylesheet primary={true} /> */}
+        {/* <NameList /> */}
         {/* <UserGreeting /> */}
         {/* <ParentComponent /> */}
         {/* <EventBind /> */}
